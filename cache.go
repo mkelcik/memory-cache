fe5f9fb331a7a538c508404beefb896355461f5d
michalkelcik <mkelcik@gmail.com>
2023-10-02 22:21:55 +0200

    add linked list to detect insert order of elements

--- cache.go
+++ cache.go
@@ -5,23 +5,30 @@ import (
 	"time"
 )
 
-type Item[T any] struct {
+type Item[K comparable, T any] struct {
 	value   T
+	key     K
 	created time.Time
+
+	prev *Item[K, T]
+	next *Item[K, T]
 }
 
 type Cache[K comparable, T any] struct {
-	storage map[K]Item[T]
+	storage map[K]*Item[K, T]
 	maxSize uint
 	lock    sync.RWMutex
 	ttl     time.Duration
+
+	first *Item[K, T]
+	last  *Item[K, T]
 }
 
 func NewCache[K comparable, T any](maxSize uint, ttl time.Duration) *Cache[K, T] {
-	s := map[K]Item[T]{}
+	s := map[K]*Item[K, T]{}
 	// preallocate map
 	if maxSize > 0 {
-		s = make(map[K]Item[T], maxSize)
+		s = make(map[K]*Item[K, T], maxSize)
 	}
 
 	return &Cache[K, T]{
@@ -36,13 +43,46 @@ func (c *Cache[K, T]) Flush() {
 	c.lock.Lock()
 	defer c.lock.Unlock()
 
-	c.storage = make(map[K]Item[T], c.maxSize)
+	c.first = nil
+	c.last = nil
+	c.storage = make(map[K]*Item[K, T], c.maxSize)
 }
 
 func (c *Cache[K, T]) Del(key K) {
 	c.lock.Lock()
 	defer c.lock.Unlock()
 
+	// rebuild my list
+	item, ok := c.storage[key]
+	if ok {
+		if item.prev != nil {
+			item.prev.next = nil
+			if item.next != nil {
+				item.prev.next = item.next
+			}
+		}
+
+		if item.next != nil {
+			item.next.prev = item.prev
+		}
+
+		// handle first and last pointers
+		if c.first != nil && c.first.key == key {
+			if c.first.next != nil {
+				c.first = c.first.next
+			} else {
+				c.first = nil
+			}
+		}
+
+		if c.last != nil && c.last.key == key {
+			if c.last.prev != nil {
+				c.last = c.last.prev
+			} else {
+				c.last = nil
+			}
+		}
+	}
 	delete(c.storage, key)
 }
 
@@ -67,9 +107,22 @@ func (c *Cache[K, T]) GetOrSet(key K, callback func() (T, bool)) (T, bool) {
 }
 
 func (c *Cache[K, T]) setToMap(key K, value T) {
-	c.storage[key] = Item[T]{
+	item := &Item[K, T]{
 		value:   value,
+		key:     key,
 		created: time.Now(),
+		prev:    c.last,
+	}
+
+	c.storage[key] = item
+
+	if c.last != nil {
+		c.last.next = item
+	}
+	c.last = item
+
+	if c.first == nil {
+		c.first = item
 	}
 }
 
@@ -80,6 +133,19 @@ func (c *Cache[K, T]) getFromMap(key K) (T, bool) {
 
 	var out T
 	return out, false
+}
+
+func (c *Cache[K, T]) SetMany(items map[K]T) {
+	if len(items) == 0 {
+		return
+	}
+
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	for k, v := range items {
+		c.setToMap(k, v)
+	}
 }
 
 func (c *Cache[K, T]) Set(key K, value T) {
--- example/main.go
+++ example/main.go
@@ -4,22 +4,99 @@ import (
 	"fmt"
 	memorycache "gitbucket.home/michal/memory-cache"
 	"sync"
+	"time"
 )
 
-func main() {
+const (
+	sampleSize = 100_000_000
+)
+
+func concurrency() {
 	cache := memorycache.NewCache[int, int](100, 0)
-	cache.Set(0, 1)
-	cache.Set(1, 2)
+
+	start := time.Now()
+
+	ch := make(chan int, 10_000)
 
 	wg := sync.WaitGroup{}
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 4; i++ {
 		wg.Add(1)
-		go func(key int) {
+		go func() {
 			defer wg.Done()
-			fmt.Println(cache.Get(key))
-			cache.Set(key, 5)
-		}(i % 2)
+			for i := range ch {
+				cache.Set(i, i)
+			}
+		}()
 	}
 
+	for i := 0; i < sampleSize; i++ {
+		ch <- i
+	}
+	close(ch)
 	wg.Wait()
+
+	fmt.Println(time.Since(start).Seconds(), "s")
+}
+
+func single() {
+	cache := memorycache.NewCache[int, int](100, 0)
+
+	start := time.Now()
+	for i := 0; i < sampleSize; i++ {
+		cache.Set(i, i)
+	}
+	fmt.Println(time.Since(start).Seconds(), "s")
+}
+
+func readSingle() {
+	cache := memorycache.NewCache[int, int](100, 0)
+
+	start := time.Now()
+	for i := 0; i < sampleSize; i++ {
+		cache.Set(i, i)
+	}
+	fmt.Println("init time", time.Since(start).Seconds(), "s")
+
+	start = time.Now()
+	for i := 0; i < sampleSize; i++ {
+		_, _ = cache.Get(i)
+	}
+	duration := time.Since(start).Seconds()
+	fmt.Println("read time", duration, "s", "(", float64(sampleSize)/duration, ")")
+}
+
+func readMulti() {
+	cache := memorycache.NewCache[int, int](100, 0)
+
+	start := time.Now()
+	for i := 0; i < sampleSize; i++ {
+		cache.Set(i, i)
+	}
+	fmt.Println("init time", time.Since(start).Seconds(), "s")
+
+	start = time.Now()
+	ch := make(chan int, sampleSize)
+	wg := sync.WaitGroup{}
+	for i := 0; i < 16; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := range ch {
+				cache.Get(i)
+			}
+		}()
+	}
+
+	for i := 0; i < sampleSize; i++ {
+		ch <- i
+	}
+	close(ch)
+	wg.Wait()
+
+	duration := time.Since(start).Seconds()
+	fmt.Println("read time", duration, "s", "(", float64(sampleSize)/duration, ")")
+}
+
+func main() {
+	readMulti()
 }
